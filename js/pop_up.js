e939663aa44a091548abb6bd251a98380e84bcdc
임용근 <ookbin1004@sungkyul.ac.kr>
2023-05-17 03:56:59 +0000

    10주차 중

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,5 +1,41 @@
+/*function pop_up(){
+	window.open("../pop_up.html","팝업테스트","width=400, height=300, top=10, left=10");
+}*/
 function pop_up(){
-	window.open("../pop_up/pop_up.html","팝업테스트","width=400, height=300, top=10, left=10");
+		var cookieCheck = getCookie("popupYN");
+	if(cookieCheck != "N"){
+		window.open("pop_up.html","팝업테스트","width=400, height=300, top=10, left=10");
+	}
+}
+
+function setCookie(name, value, expiredays){
+	var date = new Date();
+	date.setDate(date.getDate() + expiredays);
+	document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "SameSite = None; Secure";
+}
+
+function getCookie(name){
+	var cookie = document.cookie;
+	console.log("쿠키를 요청합니다.");
+	if(cookie != " "){
+		var cookie_array = cookie.split("; ");
+		for (var index in cookie_array){
+			var cookie_name = cookie_array[index].split("=");
+			
+			if(cookie_name[0] == "popupYN"){
+				return cookie_name[1];
+			}
+		}
+	}
+	return;
+}
+
+function closePopup(){
+	if (document.getElementById('check_popup').value){
+		setCookie("popupYN","N",1);
+		console.log("쿠키를 설정합니다.");
+		self.close();
+	}
 }
 
 function showclock(){
